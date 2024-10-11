3107f5972db15385f6c63ace92ae62e35401b13b
Asish-Praneeth-N <142401337+Asish-Praneeth-N@users.noreply.github.com>
2024-10-11 17:53:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const transporter = nodemailer.createTransport({
 });
 
 app.get("/",(req,res)=>{
-res,send("Hellow");
+res.send("Hellow");
 })
 app.post('/schedule-task', (req, res) => {
   const { email, task, dateTime } = req.body;
